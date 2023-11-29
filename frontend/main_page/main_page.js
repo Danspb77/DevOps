2e71862ec047a7526a24094566dfec8535a5fa09
Danya <dan.shell@bk.ru>
2023-11-29 23:15:28 +0300

    pages works

--- frontend/main_page/main_page.js
+++ frontend/main_page/main_page.js
@@ -1,45 +1,39 @@
-"use strict";
 // import {} from 
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.purchase_comment_str = exports.purchase_cost_str = exports.purchase_date_str = exports.purchase_name_str = void 0;
-const add_button = document.getElementById("add_button");
-let purchase_name_str = "";
-exports.purchase_name_str = purchase_name_str;
-let purchase_date_str = "";
-exports.purchase_date_str = purchase_date_str;
-let purchase_cost_str = '';
-exports.purchase_cost_str = purchase_cost_str;
-let purchase_comment_str = '';
-exports.purchase_comment_str = purchase_comment_str;
+var add_button = document.getElementById("add_button");
+var purchase_name_str = "";
+var purchase_date_str = "";
+var purchase_cost_str = '';
+var purchase_comment_str = '';
 add_button.addEventListener("click", function () {
-    let purchase_name = document.getElementById("purchase-name");
-    let purchase_date = document.getElementById("purchase-date");
-    let purchase_cost = document.getElementById("purchase-cost");
-    let purchase_comment = document.getElementById("purchase-comment");
-    exports.purchase_name_str = purchase_name_str = purchase_name.value;
-    exports.purchase_date_str = purchase_date_str = purchase_date.value;
-    exports.purchase_cost_str = purchase_cost_str = purchase_cost.value;
-    exports.purchase_comment_str = purchase_comment_str = purchase_comment.value;
-    alert(`Button clicked! ${purchase_name_str}`);
+    var purchase_name = document.getElementById("purchase-name");
+    var purchase_date = document.getElementById("purchase-date");
+    var purchase_cost = document.getElementById("purchase-cost");
+    var purchase_comment = document.getElementById("purchase-comment");
+    purchase_name_str = purchase_name.value;
+    purchase_date_str = purchase_date.value;
+    purchase_cost_str = purchase_cost.value;
+    purchase_comment_str = purchase_comment.value;
+    alert("Button clicked! ".concat(purchase_name_str));
     // console.log("Password:", password_str);
 });
-const day_button = document.getElementById("day_button");
-const per_day_result = document.getElementById("perDayResult");
+export { purchase_name_str, purchase_date_str, purchase_cost_str, purchase_comment_str };
+var day_button = document.getElementById("day_button");
+var per_day_result = document.getElementById("perDayResult");
 day_button.addEventListener('click', function () {
     per_day_result.textContent = "variable from import";
 });
-const week_button = document.getElementById("week_button");
-const per_week_result = document.getElementById("perDayResult");
+var week_button = document.getElementById("week_button");
+var per_week_result = document.getElementById("perDayResult");
 week_button.addEventListener('click', function () {
     per_week_result.textContent = "variable from import";
 });
-const month_button = document.getElementById("month_button");
-const per_month_result = document.getElementById("perDayResult");
+var month_button = document.getElementById("month_button");
+var per_month_result = document.getElementById("perDayResult");
 month_button.addEventListener('click', function () {
     per_month_result.textContent = "variable from import";
 });
-const year_button = document.getElementById("year_button");
-const per_year_result = document.getElementById("perDayResult");
+var year_button = document.getElementById("year_button");
+var per_year_result = document.getElementById("perDayResult");
 year_button.addEventListener('click', function () {
     per_year_result.textContent = "variable from import";
 });
--- frontend/sign_up_page/sign_up.js
+++ frontend/sign_up_page/sign_up.js
@@ -1,21 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.password_str = exports.email_str = exports.username_str = void 0;
 // Update your TypeScript code to use ES6 modules
-const button = document.getElementById("sign_up_button");
-let username_str = "";
-exports.username_str = username_str;
-let email_str = "";
-exports.email_str = email_str;
-let password_str = "";
-exports.password_str = password_str;
+var button = document.getElementById("sign_up_button");
+var username_str = "";
+var email_str = "";
+var password_str = "";
 button.addEventListener("click", function () {
-    let username = document.getElementById("username");
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-    exports.username_str = username_str = username.value;
-    exports.email_str = email_str = email.value;
-    exports.password_str = password_str = password.value;
-    alert(`Button clicked! ${username_str}`);
+    var username = document.getElementById("username");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    username_str = username.value;
+    email_str = email.value;
+    password_str = password.value;
+    alert("Button clicked! ".concat(username_str));
     console.log("Password:", password_str);
 });
+// Export individual variables instead of using exports
+export { username_str, email_str, password_str };
