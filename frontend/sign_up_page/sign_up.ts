3c7857aa72c51165bf713f399b69e8368e082f3e
Danya <dan.shell@bk.ru>
2023-11-30 10:45:10 +0300

    add request module in sign up script, but it doesn't worked

--- frontend/sign_up_page/sign_up.ts
+++ frontend/sign_up_page/sign_up.ts
@@ -1,22 +1,43 @@
-// Update your TypeScript code to use ES6 modules
 const button = document.getElementById("sign_up_button") as HTMLButtonElement;
 
-let username_str: string = "";
-let email_str: string = "";
-let password_str: string = "";
-
 button.addEventListener("click", function() {
-    let username = document.getElementById("username") as HTMLInputElement;
-    let email = document.getElementById("email") as HTMLInputElement;
-    let password = document.getElementById("password") as HTMLInputElement;
+    const username = document.getElementById("username") as HTMLInputElement;
+    const email = document.getElementById("email") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
 
-    username_str = username.value;
-    email_str = email.value;
-    password_str = password.value;
+    const username_str = username.value;
+    const email_str = email.value;
+    const password_str = password.value;
+
+    const dataToSend = {
+        login: username_str,
+        password: password_str,
+        password_repeat: password_str,
+    };
+
+    sendData(dataToSend);
 
     alert(`Button clicked! ${username_str}`);
     console.log("Password:", password_str);
 });
 
-// Export individual variables instead of using exports
-export { username_str, email_str, password_str };
+async function sendData(dataToSend: any):Promise<void> {
+    try {
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dataToSend),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        console.log('Response Data:', responseData);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
